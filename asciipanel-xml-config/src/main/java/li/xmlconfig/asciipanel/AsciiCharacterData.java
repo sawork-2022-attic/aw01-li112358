ba4d02a04a01ae6182d678599614a014971b89f1
li112358 <1311590726@qq.com>
2022-03-02 08:52:58 +0800

    finish-xml

---
+++ asciipanel-xml-config/src/main/java/li/xmlconfig/asciipanel/AsciiCharacterData.java
@@ -0,0 +1,9 @@
+package li.xmlconfig.asciipanel;
+
+import java.awt.Color;
+
+public class AsciiCharacterData {
+	public char character;
+	public Color foregroundColor;
+	public Color backgroundColor;
+}
